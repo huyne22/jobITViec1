e8262d5f6b666ed6d1e1d27ecbb0f7c08f0df040
your name <luonghuy148894@gmail.com>
2024-04-25 16:52:35 +0700

    push main

--- jobCV/src/components/client/footer.client.tsx
+++ jobCV/src/components/client/footer.client.tsx
@@ -2,7 +2,6 @@
 const Footer = () => {
     return (
         <footer style={{ padding: 15, textAlign: "center" }}>
-            {/* <div>Frontend React Typescript - Series Nest.JS Basic &copy; Hỏi Dân IT</div> */}
         </footer>
     )
 }
